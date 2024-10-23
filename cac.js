330e84e012c61f4ff96ea8ed8a053d663a03305a
ArunMut <arunadarsh.m@gmail.com>
2024-10-23 02:44:36 -0500

    fadsfasd

--- cac.js
+++ cac.js
@@ -20,5 +20,66 @@ app.use("/volunteer", express.static(path.join(__dirname, "/public/volunteer/"))
 app.use("/petitions", express.static(path.join(__dirname, "/public/petitions/")));
 
 httpserver.listen(3000, () => {
-	console.log("Server Started");
+	console.log("Server Started")
 });
+
+//actual code
+app.get("/content/:name", (req, res) => {
+	res.sendFile(path.join(__dirname, "/public/content/", req.params.name));
+})
+
+app.post("/vData", (req, res) => {
+	res.send(JSON.stringify(db.get("volunteer")));
+})
+app.post("/pData", (req, res) => {
+	res.send(JSON.stringify(db.get("petitions")));
+})
+
+app.get("/addPost/:title/:desc/:loc/:date/:time/:img",(req,res)=>{
+	let a = db.get("volunteer")
+	a.push({
+		"title": req.params.title,
+		"desc": req.params.desc,
+		"loc": req.params.loc,
+		"date": req.params.date,
+		"time": req.params.time,
+		"img": decodeURIComponent(req.params.img),
+		"ts": Date.now(),
+		"signups":[]
+	})
+	db.set("volunteer",a)
+})
+
+app.get("/addPet/:title/:desc/:img",(req,res)=>{
+	let a = db.get("petitions")
+	a.push({
+		"title": req.params.title,
+		"desc": req.params.desc,
+		"img": decodeURIComponent(req.params.img),
+		"ts": Date.now(),
+		"votes":[],
+		"count": 0
+	})
+	db.set("petitions",a)
+})
+
+app.get("/subscribeToPost/:postname/:email", (req,res)=>{
+	let a = db.get("volunteer")
+	for (let i of a){
+		if (i.title == req.params.postname){
+			i.signups.push(req.params.email)
+		}
+	}
+	db.set("volunteer",a)
+})
+
+app.get("/vote/:postname/:email", (req,res)=>{
+	let a = db.get("petitions")
+	for (let i of a){
+		if (i.title == req.params.postname){
+			i.votes.push(req.params.email)
+			i.count++
+		}
+	}
+	db.set("petitions",a)
+})
